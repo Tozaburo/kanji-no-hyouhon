7c5808d5ade1defcc66af5ee051fdb374869e1fd
Tozaburo <85058495+Tozaburo@users.noreply.github.com>
2023-08-19 19:42:00 +0900

    Update script.js

--- script.js
+++ script.js
@@ -8,6 +8,50 @@ var collectedKanji = [];
 var flattenKanji = [];
 var answerKanji = "";
 var onKanji = [];
+var kunKanji = [];
+var collectedKanjiNum = 0;
+
+for (var n = 0; n < Number(level) + 1; n++) {
+    flattenKanji = flattenKanji.concat(kanji[n]);
+}
+
+var cookies = getCookiesAsArray();
+collected = "";
+if (!cookies[0].includes("collected")) {
+    makeCookie("collected", "10000000000000000,100000000000000000000000000000000,10000000000000000000000000000000000000000,40000000000000000000000000000000000000000,800000000000000000000000000000000000000,200000000000000000000000000000000000000,800000000000000000000000000000000000000000000000000000000000000,g00000000000000000000000000000000000000000000000000000000,800000000000000000000000000000000000000000000000000000000000000000,80000000000000000000000000000000000000,200000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000,0");
+    collected = ["10000000000000000", "100000000000000000000000000000000", "10000000000000000000000000000000000000000", "40000000000000000000000000000000000000000", "800000000000000000000000000000000000000", "200000000000000000000000000000000000000", "800000000000000000000000000000000000000000000000000000000000000", "g00000000000000000000000000000000000000000000000000000000", "800000000000000000000000000000000000000000000000000000000000000000", "80000000000000000000000000000000000000", "200000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000", "0"];
+} else {
+    collected = cookies[1][cookies[0].indexOf("collected")].split(",");
+}
+if (!cookies[0].includes("level")) {
+    makeCookie("level", "0");
+    level = "0";
+} else {
+    level = cookies[1][cookies[0].indexOf("level")];
+}
+
+collectedKanji = [];
+for (var n = 0; n < collected.length; n++) {
+    var tck = convertBase(collected[n], 32, 2);
+    var repeat = kanji[n].length - tck.length + 1;
+    for (var m = 0; m < repeat; m++) {
+        tck += "0";
+    }
+    collectedKanji.push(tck);
+}
+saveCollected();
+
+levelKanji = [];
+for (var n = 0; n < Number(level) + 1; n++) {
+    levelKanji.push([]);
+    for (var m = 0; m < Number(kanji[n].length); m++) {
+        levelKanji[levelKanji.length - 1].push(kanji[n][m][0]);
+    }
+}
+
+for (var n = 0; n < Number(level) + 1; n++) {
+    flattenKanji = flattenKanji.concat(kanji[n]);
+}
 
 window.addEventListener("DOMContentLoaded", function () {
     if (location.href.includes("#")) {
@@ -18,44 +62,14 @@ window.addEventListener("DOMContentLoaded", function () {
             location.href = url[0];
         }
     }
-    var cookies = getCookiesAsArray();
-    collected = "";
-    if (!cookies[0].includes("collected")) {
-        makeCookie("collected", "0,0,0,0,0,0,0,0,0,0,0,0");
-        collected = ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"];
-    } else {
-        collected = cookies[1][cookies[0].indexOf("collected")].split(",");
-    }
-    if (!cookies[0].includes("level")) {
-        makeCookie("level", "1");
-        level = "1";
-    } else {
-        level = cookies[1][cookies[0].indexOf("level")];
-    }
-    // collected = ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"];
-    // level = "2";
-
-    collectedKanji = [];
-    for (var n = 0; n < collected.length; n++) {
-        var tck = Number(parseInt(collected[n], 36)).toString(2);
-        var repeat = kanji[n].length - tck.length;
-        for (var m = 0; m < repeat; m++) {
-            tck += "0";
-        }
-        collectedKanji.push(tck);
-    }
-
-    levelKanji = [];
-    for (var n = 0; n < Number(level); n++) {
-        levelKanji.push([]);
-        for (var m = 0; m < Number(kanji[n].length); m++) {
-            levelKanji[levelKanji.length - 1].push(kanji[n][m][0]);
-        }
-    }
 });
 
+function convertBase(numStr, fromBase, toBase) {
+    var decimalNumber = parseInt(numStr, fromBase);
+    return decimalNumber.toString(toBase);
+}
+
 function showSection(id) {
-    flattenKanji = kanji[0].concat(kanji[1]);
     if (currentSection === id) return;
     document.getElementById(currentSection).classList.add('hidden');
     setTimeout(() => {
@@ -67,24 +81,30 @@ function showSection(id) {
             loadList();
         }
         if (id == "collect") {
-            document.querySelector("#class").innerHTML = "現在の級：" + kankenLevel[Number(level) - 1] + "級";
+            document.querySelector("#class").innerHTML = "現在の級：" + kankenLevel[Number(level)] + "級";
         }
         if (id == "on") {
             onQuestion();
         }
+        if (id == "kun") {
+            kunQuestion();
+        }
         if (id == "correct") {
-            document.querySelector("#on-kanji-description").innerHTML = "「" + answerKanji + "」をゲットしました！"
+            document.querySelector("#correct-description").innerHTML = "「" + answerKanji + "」をゲットしました！"
             for (var n = 0; n < kanji.length; n++) {
                 for (var m = 0; m < kanji[n].length; m++) {
                     if (kanji[n][m][0] == answerKanji) {
-                        console.log(collectedKanji)
-                        collectedKanji.splice('' + n, 1, collectedKanji[n].substr(0, m) + 1 + collectedKanji[n].substr(m + 1))
+                        collectedKanji[n] = replaceAt(collectedKanji[n], m + 1, "1");
                         saveCollected();
-                        console.log(collectedKanji)
                         break;
                     }
                 }
             }
+        }
+        if (id == "status") {
+            countCollected();
+            document.querySelector("#status-class").innerHTML = "現在の級：" + kankenLevel[Number(level)] + "級";
+            document.querySelector("#status-next-class").innerHTML = "次級に行くために必要な漢字：" + String(collectedKanjiNum) + "/" + String(flattenKanji.length);
         }
     }, 500);
 }
@@ -111,55 +131,28 @@ function getCookiesAsArray() {
 function saveCollected() {
     collected = [];
     for (var n = 0; n < collectedKanji.length; n++) {
-        collected.splice(n, 1, parseInt(collectedKanji[n], 2).toString(32));
+        collected.push(convertBase(collectedKanji[n], 2, 32));
     }
     makeCookie("collected", collected.join(","));
-    console.log(collected.join(","))
+}
+
+function countCollected() {
+    collectedKanjiNum = 0;
+    for (var n = 0; n < levelKanji.length; n++) {
+        for (var m = 0; m < levelKanji[n].length; m++) {
+            if (collectedKanji[n][m + 1] != 1) {
+                collectedKanjiNum += 1;
+            }
+        }
+    }
 }
 
 function loadList() {
-    var cookies = getCookiesAsArray();
-    collected = "";
-    if (!cookies[0].includes("collected")) {
-        makeCookie("collected", "0,0,0,0,0,0,0,0,0,0,0,0");
-        collected = ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"];
-    } else {
-        collected = cookies[1][cookies[0].indexOf("collected")].split(",");
-    }
-    if (!cookies[0].includes("level")) {
-        makeCookie("level", "1");
-        level = "1";
-    } else {
-        level = cookies[1][cookies[0].indexOf("level")];
-    }
-    // collected = ["0", "50", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"];
-    // level = "2";
-
-    collectedKanji = [];
-    for (var n = 0; n < collected.length; n++) {
-        var tck = Number(parseInt(collected[n], 36)).toString(2);
-        var repeat = kanji[n].length - tck.length;
-        for (var m = 0; m < repeat; m++) {
-            tck += "0";
-        }
-        collectedKanji.push(tck);
-    }
-
-    levelKanji = [];
-    for (var n = 0; n < Number(level); n++) {
-        levelKanji.push([]);
-        for (var m = 0; m < Number(kanji[n].length); m++) {
-            levelKanji[levelKanji.length - 1].push(kanji[n][m][0]);
-        }
-    }
-
-    var count = 0;
     for (var n = 0; n < levelKanji.length; n++) {
         for (var m = 0; m < levelKanji[n].length; m++) {
-            if (collectedKanji[n][m] != 1) {
+            if (collectedKanji[n][m + 1] != 1) {
                 levelKanji[n].splice(m, 1, "？");
             }
-            count += 1;
         }
     }
 
@@ -170,70 +163,130 @@ function loadList() {
             document.querySelector("#kanji-list").innerHTML += "<li><div class='line'></div></li>";
         }
         for (var m = 0; m < levelKanji[n].length; m++) {
-            document.querySelector("#kanji-list").innerHTML += "<li><a href='#kanjiDescription' onclick='kanjiDescription(" + count + ")'>" + levelKanji[n][m] + "</a></li>";
+            document.querySelector("#kanji-list").innerHTML += "<li><a onclick='kanjiDescription(" + count + ")'>" + levelKanji[n][m] + "</a></li>";
             count += 1;
         }
     }
 }
 
 function kanjiDescription(num) {
+    var flattenLevelKanji = [];
+    for (var n = 0; n < Number(level) + 1; n++) {
+        flattenLevelKanji = flattenLevelKanji.concat(levelKanji[n]);
+    }
     showSection("kanjiDescription");
-    flattenKanji = kanji[0].concat(kanji[1]);
-    document.querySelector("#kanjiDescriptionTitle").innerHTML = "漢字「" + flattenKanji[num][0] + "」について";
-    document.querySelector("#kanji-description").innerHTML = "漢字：" + flattenKanji[num][0];
-    if (flattenKanji[num][3].includes("・")) {
-        document.querySelector("#on-description").innerHTML = "音読み：" + flattenKanji[num][3].join("・");
+    console.log(flattenLevelKanji[num])
+    if (flattenLevelKanji[num] == "？") {
+        document.querySelector("#kanjiDescription").innerHTML = `
+<h1 class="title" id="kanjiDescriptionTitle">まだこの漢字をゲットしていないようです...</h1>
+<ul class="section-menu">
+    <li>
+        <a href="#main" onclick="showSection('main')">ホーム</a>
+    </li>
+    <li>
+    <a href="#main" onclick="window.location.reload();">戻る</a>
+    </li>
+</ul>`;
     } else {
-        document.querySelector("#on-description").innerHTML = "音読み：" + flattenKanji[num][3];
+        document.querySelector("#kanjiDescription").innerHTML = `
+<div class="list-title">
+    <h1 class="title" id="kanjiDescriptionTitle">漢字「」について</h1>
+    <a href="#main" class="home" onclick="showSection('main')">ホーム</a>
+    <a href="#main" class="home" onclick="window.location.reload();">戻る</a>
+</div>
+<div class="descriptionContainer">
+    <p id="kanji-description">漢字：</p>
+    <p id="on-description">音読み：</p>
+    <p id="kun-description">訓読み：</p>
+    <p id="bushu-description">部首：</p>
+    <p id="kakusu-description">画数：</p>
+</div>`
+        document.querySelector("#kanjiDescriptionTitle").innerHTML = "漢字「" + flattenKanji[num][0] + "」について";
+        document.querySelector("#kanji-description").innerHTML = "漢字：" + flattenKanji[num][0];
+        if (typeof flattenKanji[num][3] == "object") {
+            document.querySelector("#on-description").innerHTML = "音読み：" + flattenKanji[num][3].join("・");
+        } else {
+            document.querySelector("#on-description").innerHTML = "音読み：" + flattenKanji[num][3];
+        }
+        if (typeof flattenKanji[num][4] == "object") {
+            document.querySelector("#kun-description").innerHTML = "訓読み：" + flattenKanji[num][4].join("・");
+        } else {
+            document.querySelector("#kun-description").innerHTML = "訓読み：" + flattenKanji[num][4];
+        }
+        document.querySelector("#bushu-description").innerHTML = "部首：" + flattenKanji[num][1];
+        document.querySelector("#kakusu-description").innerHTML = "画数：" + flattenKanji[num][2];
     }
-    if (flattenKanji[num][4].includes("・")) {
-        document.querySelector("#kun-description").innerHTML = "訓読み：" + flattenKanji[num][4].join("・");
-    } else {
-        document.querySelector("#kun-description").innerHTML = "訓読み：" + flattenKanji[num][4];
-    }
-    document.querySelector("#bushu-description").innerHTML = "部首：" + flattenKanji[num][1];
-    document.querySelector("#kakusu-description").innerHTML = "画数：" + flattenKanji[num][2];
 }
 
 function arrayShuffle(array) {
-    for (let i = (array.length - 1); 0 < i; i--) {
+    for (var n = (array.length - 1); 0 < n; n--) {
 
-        let r = Math.floor(Math.random() * (i + 1));
+        var r = Math.floor(Math.random() * (n + 1));
 
-        let tmp = array[i];
-        array[i] = array[r];
+        var tmp = array[n];
+        array[n] = array[r];
         array[r] = tmp;
     }
     return array;
 }
 
 function onQuestion() {
-    var randomAnswer = Math.floor(Math.random() * flattenKanji.length);
-    answerKanji = flattenKanji[randomAnswer][0]
-    if (typeof flattenKanji[randomAnswer][3] == "object") {
-        var answerYomi = flattenKanji[randomAnswer][3][Math.floor(Math.random() * flattenKanji[randomAnswer][3].length)];
-    } else {
-        var answerYomi = flattenKanji[randomAnswer][3];
+    var answerYomi = "";
+    while (answerYomi == "") {
+        var randomAnswer = Math.floor(Math.random() * flattenKanji.length);
+        answerKanji = flattenKanji[randomAnswer][0]
+        if (typeof flattenKanji[randomAnswer][3] == "object") {
+            var answerYomi = flattenKanji[randomAnswer][3][Math.floor(Math.random() * flattenKanji[randomAnswer][3].length)];
+        } else {
+            answerYomi = flattenKanji[randomAnswer][3];
+        }
     }
     var random = 0;
     onKanji = [];
     onKanji.push(answerKanji);
     for (var n = 0; n < 8;) {
         random = Math.floor(Math.random() * flattenKanji.length);
-        if (!flattenKanji[random][3].includes(answerYomi)) {
+        if (!flattenKanji[random][3].includes(answerYomi) && !onKanji.includes(flattenKanji[random][0])) {
             onKanji.push(flattenKanji[random][0])
             n += 1;
         }
 
     }
-    console.log(answerKanji)
-    console.log(answerYomi)
     onKanji = arrayShuffle(onKanji);
-    console.log(onKanji);
-    document.querySelector("#on-kanji-question").innerHTML = "次のうち「" + answerYomi + "」と読むものを1つ選べ";
+    document.querySelector("#on-kanji-question").innerHTML = "次のうち「" + answerYomi + "」と音読みで読むものを1つ選べ";
     document.querySelector("#on-kanji-list").innerHTML = "";
     for (var n = 0; n < 9; n++) {
         document.querySelector("#on-kanji-list").innerHTML += "<li><a onclick='onKanjiJudgment(" + n + ")'>" + onKanji[n] + "</a></li>";
+    }
+}
+
+function kunQuestion() {
+    var answerYomi = "";
+    while (answerYomi == "") {
+        var randomAnswer = Math.floor(Math.random() * flattenKanji.length);
+        answerKanji = flattenKanji[randomAnswer][0]
+        if (typeof flattenKanji[randomAnswer][4] == "object") {
+            var answerYomi = flattenKanji[randomAnswer][4][Math.floor(Math.random() * flattenKanji[randomAnswer][4].length)];
+        } else {
+            answerYomi = flattenKanji[randomAnswer][4];
+        }
+    }
+    var random = 0;
+    kunKanji = [];
+    kunKanji.push(answerKanji);
+    for (var n = 0; n < 8;) {
+        random = Math.floor(Math.random() * flattenKanji.length);
+        if (!flattenKanji[random][4].includes(answerYomi) && !kunKanji.includes(flattenKanji[random][0])) {
+            kunKanji.push(flattenKanji[random][0])
+            n += 1;
+        }
+
+    }
+    kunKanji = arrayShuffle(kunKanji);
+    document.querySelector("#kun-kanji-question").innerHTML = "次のうち「" + answerYomi + "」と訓読みで読むものを1つ選べ";
+    document.querySelector("#kun-kanji-list").innerHTML = "";
+    for (var n = 0; n < 9; n++) {
+        document.querySelector("#kun-kanji-list").innerHTML += "<li><a onclick='kunKanjiJudgment(" + n + ")'>" + kunKanji[n] + "</a></li>";
     }
 }
 
@@ -243,4 +296,21 @@ function onKanjiJudgment(num) {
     } else {
         showSection("wrong");
     }
+}
+
+function kunKanjiJudgment(num) {
+    if (answerKanji == kunKanji[num]) {
+        showSection("correct");
+    } else {
+        showSection("wrong");
+    }
+}
+
+function replaceAt(str, index, replacement) {
+    return str.substr(0, index) + replacement + str.substr(index + replacement.length);
+}
+
+function goBack() {
+    history.back(-1)
+    window.location.reload();
 }
